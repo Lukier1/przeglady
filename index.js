fe116a1d3a68bac7f5ed4ad811582f3a39179be2
Łukasz Kowalczyk <lukier1@gmail.com>
2018-10-28 17:13:11 +0100

    Adding express

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+const app = express();
+const port = process.env.PORT != null ? process.env.PORT : 8081;
+
+app.get('api/', function (req, res) {
+   res.send('Hello world');
+});
+
+app.use(express.static('public'));
+
+var server = app.listen(port, () =>  console.log(`Przeglad app start on port: ${port}`));
